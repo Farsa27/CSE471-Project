00ac47f429ab32e6189e39badf8b041797e4546a
Samia Rahman Mridula <mridulasamia4@gmail.com>
2026-01-02 06:06:28 +0600

    Update lostItemForm.jsx

--- client/src/components/lostItemForm.jsx
+++ client/src/components/lostItemForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import './lostitemForm.css';
+import './lostItemForm.css';
 import { useTranslation } from 'react-i18next';
 
 export default function LostItemForm({ onCreated }) {
